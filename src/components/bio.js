415a78ef6b8de58c01ddd7f303ca47a70626cedc
Kaustav Banerjee <35100716+Youngwolf1810@users.noreply.github.com>
2020-10-25 17:30:53 +0530

    profile pic uploaded

--- src/components/bio.js
+++ src/components/bio.js
@@ -16,8 +16,8 @@ const Bio = () => {
     query BioQuery {
       avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
         childImageSharp {
-          fixed(width: 100, height: 100) {
-            ...GatsbyImageSharpFixed
+          fluid {
+            ...GatsbyImageSharpFluid
           }
         }
       }
